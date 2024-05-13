b4d04bfc6cb0d92518f7408a4178a28cd1b55275
SeratiVilankulu <seratimotla@gmail.com>
2024-05-13 09:14:41 +0000

    More Data Structures

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    value = 0
-    if not (isinstance(roman_string, str)):
-        return (0)
-    for i in range(len(roman_string)):
-        if (roman_string[i] == 'I'):
-            value += 1
-        if (roman_string[i] == 'V'):
-            if (roman_string[i - 1] == 'I' and i != 0):
-                value -= 2
-            value += 5
-        if (roman_string[i] == 'X'):
-            if (roman_string[i - 1] == 'I' and i != 0):
-                value -= 2
-            value += 10
-        if (roman_string[i] == 'L'):
-            if (roman_string[i - 1] == 'X' and i != 0):
-                value -= 20
-            value += 50
-        if (roman_string[i] == 'C'):
-            if (roman_string[i - 1] == 'X' and i != 0):
-                value -= 20
-            value += 100
-        if (roman_string[i] == 'D'):
-            if (roman_string[i - 1] == 'C' and i != 0):
-                value -= 200
-            value += 500
-        if (roman_string[i] == 'M'):
-            if (roman_string[i - 1] == 'C' and i != 0):
-                value -= 200
-            value += 1000
-    return (value)
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(roman_string):
+        value = roman_values[char]
+        
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        
+        prev_value = value
+    
+    return total
