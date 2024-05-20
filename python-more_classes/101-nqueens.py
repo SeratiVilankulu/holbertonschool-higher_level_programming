fbebb6446eee871ed349009149a82d69441f2404
SeratiVilankulu <seratimotla@gmail.com>
2024-05-20 09:50:58 +0000

    N queens

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 
-
 def print_NQueen(square_y, N):
-    """Create a the format and print the result
+    """Create the format and print the result
     result = possible variant of the queen position
     in the chess game"""
     result = []
-    i = 0
-    for a in square_y:
-        new = []
-        new.append(i)
-        new.append(a)
-        result.append(new)
-        i = i + 1
+    for i in range(N):
+        result.append([i, square_y[i]])
     print(result)
 
-
 def check_move(pos_x, square_y):
-    """Check if the square it is available to move or not
-        we check the position between one queen and the
-        posible position of the new queen"""
+    """Check if the square is available to move or not.
+    We check the position between one queen and the
+    possible position of the new queen"""
     for i in range(pos_x):
         if square_y[i] == square_y[pos_x]:
             return False
@@ -27,20 +20,19 @@ def check_move(pos_x, square_y):
             return False
     return True
 
-
 def solve_NQueen(N, pos_x, square_y):
     """Backtracking function:
     - N: Size of chessboard
     - pos_x: actual position
-    - square_x: list of solutions to move the queen"""
-    if (pos_x == N):
+    - square_y: list of solutions to move the queen"""
+    if pos_x == N:
         print_NQueen(square_y, N)
     else:
         for i in range(N):
             square_y.append(i)
-            if (check_move(pos_x, square_y)):
+            if check_move(pos_x, square_y):
                 solve_NQueen(N, pos_x + 1, square_y)
-            square_y.pop(-1)
+            square_y.pop()
 
 if __name__ == '__main__':
     import sys
@@ -53,15 +45,16 @@ if __name__ == '__main__':
 
     try:
         N = int(N)
-    except:
+    except ValueError:
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
 
-    if (N < 4):
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    # initialize chess game
+    # Initialize chess game
     square_y = []
     pos_x = 0
     solve_NQueen(N, pos_x, square_y)
+
