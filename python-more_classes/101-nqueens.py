681affa630133d803d6db816aa751b4b7c1d305b
SeratiVilankulu <seratimotla@gmail.com>
2024-05-20 10:00:54 +0000

    N queens

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -9,7 +9,7 @@ def print_NQueen(square_y, N):
         result.append([i, square_y[i]])
     print(result)
 
-def check_move(pos_x, square_y):
+ def check_move(pos_x, square_y):
     """Check if the square is available to move or not.
     We check the position between one queen and the
     possible position of the new queen"""
@@ -20,7 +20,7 @@ def check_move(pos_x, square_y):
             return False
     return True
 
-def solve_NQueen(N, pos_x, square_y):
+ def solve_NQueen(N, pos_x, square_y):
     """Backtracking function:
     - N: Size of chessboard
     - pos_x: actual position
@@ -34,7 +34,7 @@ def solve_NQueen(N, pos_x, square_y):
                 solve_NQueen(N, pos_x + 1, square_y)
             square_y.pop()
 
-if __name__ == '__main__':
+ if __name__ == '__main__':
     import sys
 
     if len(sys.argv) != 2:
@@ -57,4 +57,3 @@ if __name__ == '__main__':
     square_y = []
     pos_x = 0
     solve_NQueen(N, pos_x, square_y)
-
