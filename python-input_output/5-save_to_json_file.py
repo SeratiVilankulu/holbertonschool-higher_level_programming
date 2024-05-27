8d8368323c29d2b5b81ef0c46423930e0e445c94
SeratiVilankulu <seratimotla@gmail.com>
2024-05-27 06:52:22 +0000

    Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,12 +5,8 @@ Contains a function that writes an Object to a text file.
 """
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation."""
-    try:
-        with open(filename, 'w') as f:
-            json.dump(my_obj, f)
-    except TypeError as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-    except PermissionError as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
