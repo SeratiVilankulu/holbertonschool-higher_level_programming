8daf3aa717c66d2a0985c4e6911be4c3511fabb3
SeratiVilankulu <160496471+SeratiVilankulu@users.noreply.github.com>
2024-05-31 09:02:36 +0200

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,7 +4,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # Define the request handler class
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    
+
     def do_GET(self):
         # Check the path and respond accordingly
         if self.path == '/':
@@ -12,7 +12,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        
+
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -23,16 +23,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 "city": "New York"
             }
             self.wfile.write(json.dumps(data).encode())
-        
+
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             status = {
-                "status" : "OK"
+                "status": "OK"
             }
             self.wfile.write(json.dumps(status).encode())
-        
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
