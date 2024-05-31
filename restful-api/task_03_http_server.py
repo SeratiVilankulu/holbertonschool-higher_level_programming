1e00854662b2a219aecb2a533855633443c7e192
SeratiVilankulu <160496471+SeratiVilankulu@users.noreply.github.com>
2024-05-31 08:57:38 +0200

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -24,12 +24,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(data).encode())
         
-        elif self.path == '/status':
+        elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             status = {
-                "status": "OK"
+                "version": "1.0", "description": "A simple API built with http.server"
             }
             self.wfile.write(json.dumps(status).encode())
         
