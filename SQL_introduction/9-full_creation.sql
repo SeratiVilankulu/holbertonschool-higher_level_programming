468369c9e5ed3c982319c9707da6592ff7a55377
SeratiVilankulu <seratimotla@gmail.com>
2024-06-18 07:32:01 +0000

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- Create new table with data
-CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
-INSERT INTO second_table (id, name. score) values (1, "John", 10);
-INSERT INTO second_table (id, name. score) values (2, "Alex", 3);
-INSERT INTO second_table (id, name. score) values (3, "Bob", 14);
-INSERT INTO second_table (id, name. score) values (4, "George", 8);
+-- CREATE SECOND TABLE and insert into
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
+INSERT INTO second_table (id, name, score) value (1, 'John', 10);
+INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) value (4, 'George', 8);
