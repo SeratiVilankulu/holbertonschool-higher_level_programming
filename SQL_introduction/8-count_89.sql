b7bda4b06246f702d65f7e08610054709379cb86
SeratiVilankulu <seratimotla@gmail.com>
2024-06-18 07:24:20 +0000

    Count 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Display number of records with the same id
-SELECT COUNT (*) FROM first_table WHERE id = 89;
+-- number of record of id in table 
+SELECT COUNT(id) FROM first_table Where id = 89;
