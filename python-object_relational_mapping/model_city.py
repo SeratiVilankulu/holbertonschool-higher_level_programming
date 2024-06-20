0fa4684f6f3a8a23b6bf3d8bd5d96c03c0938e07
SeratiVilankulu <seratimotla@gmail.com>
2024-06-20 12:45:27 +0000

    Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-Defines the City class which links to the MySQL table 'cities'.
+Write a python file that contains the class
+definition of a City
 """
-
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from model_state import Base
 
-# Define the City class which will be linked to the 'cities' table
+
 class City(Base):
+    """
+        City class
+    """
     __tablename__ = 'cities'
-
-    # Define the columns in the 'cities' table
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
