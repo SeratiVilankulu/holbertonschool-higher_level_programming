748d33838792ce838db0e20c427e79e44ffaabce
SeratiVilankulu <seratimotla@gmail.com>
2024-06-20 13:01:55 +0000

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,41 +1,25 @@
+
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa.
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+script that lists all State objects
+from the database hbtn_0e_6_usa"""
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # Ensure the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./7-model_state_fetch_all.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create an engine connected to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}', pool_pre_ping=True)
-
-    # Bind the engine to the metadata of the Base class so that the declaratives can be accessed through a DBSession instance
-    Base.metadata.bind = engine
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query all State objects, ordered by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Print each state in the required format
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
     session.close()
