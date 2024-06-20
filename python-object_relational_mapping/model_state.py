648b83ff366c467fe866dc7b054ccb52c06953f2
SeratiVilankulu <seratimotla@gmail.com>
2024-06-20 13:04:09 +0000

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
 """
-Defines the State class and creates a SQLAlchemy ORM model that links to the MySQL table 'states'.
+Write a python file that contains the class
+definition of a State
 """
-
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-# Create a base class for declarative class definitions
 Base = declarative_base()
+
 
 class State(Base):
     """
-    State class that maps to the 'states' table in the MySQL database.
+        State class
     """
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-# Only run the following block if this script is executed directly
-if __name__ == "__main__":
-    import sys
-
-    # Ensure correct number of arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./model_state.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create an engine connected to the specified MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}', pool_pre_ping=True)
-
-    # Create all tables in the database (this is equivalent to "CREATE TABLE" in raw SQL)
-    Base.metadata.create_all(engine)
-
