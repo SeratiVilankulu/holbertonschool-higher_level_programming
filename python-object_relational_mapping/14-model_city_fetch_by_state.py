57ba92d9a1330e1ff72b4df37e946e9b99e9aae6
SeratiVilankulu <seratimotla@gmail.com>
2024-06-20 12:35:39 +0000

    Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python3
 """
-Fetches and prints all City objects from the database hbtn_0e_14_usa,
-sorted by cities.id, in the format <state name>: (<city id>) <city name>.
-"""
-
+deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa"""
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 from model_city import City
-import sys
 
 if __name__ == "__main__":
-    # Ensure the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create an engine connected to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}', pool_pre_ping=True)
-
-    # Bind the engine to the metadata of the Base class
-    Base.metadata.bind = engine
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    try:
-        # Query all City objects and join with State to get the state name
-        cities = session.query(City).join(State).order_by(City.id).all()
-
-        # Print the results in the specified format
-        for city in cities:
-            print(f"{city.state.name}: ({city.id}) {city.name}")
-
-    except Exception as e:
-        print(f"Error occurred: {e}")
-
-    finally:
-        # Close the session
-        session.close()
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    re = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id).all()
+    for C, x in re:
+        print("{}: ({}) {}".format(x.name, C.id, C.name))
+    session.commit()
+    session.close()
