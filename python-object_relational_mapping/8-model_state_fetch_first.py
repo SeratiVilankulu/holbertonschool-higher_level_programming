2f0df163080f798a5234881f711518ca9be9f76c
SeratiVilankulu <seratimotla@gmail.com>
2024-06-20 13:00:09 +0000

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,43 +1,27 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the database hbtn_0e_6_usa.
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+script that lists all State objects
+from the database hbtn_0e_6_usa"""
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # Ensure the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create an engine connected to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}', pool_pre_ping=True)
-
-    # Bind the engine to the metadata of the Base class
-    Base.metadata.bind = engine
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query the first State object ordered by id
-    state = session.query(State).order_by(State.id).first()
-
-    # Print the result in the required format
-    if state:
-        print(f"{state.id}: {state.name}")
-    else:
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    re = session.query(State).order_by(State.id).first()
+    if re is None:
         print("Nothing")
-
-    # Close the session
+    else:
+        print("{}: {}".format(re.id, re.name))
     session.close()
