7288fa1523cd0b6272711bc915c64555fcac4571
SeratiVilankulu <seratimotla@gmail.com>
2024-07-19 12:18:58 +0000

    Creating a Basic HTML Template in Flask

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import json
 
 app = Flask(__name__)
 
@@ -14,21 +13,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-@app.route('/items')
-def items():
-    # Read the items from the JSON file
-    try:
-        with open('items.json') as file:
-            data = json.load(file)
-            items_list = data.get('items', [])
-    except FileNotFoundError:
-        items_list = []
-    except json.JSONDecodeError:
-        items_list = []
-
-    # Render the template with the list of items
-    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
